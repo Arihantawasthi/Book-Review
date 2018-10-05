9d64882085e216b33d5981e14f6df60bcc16c6e1
Arihantawasthi <awasthiarihant@gmail.com>
2018-10-06 03:55:22 +0530

    Added login and register

--- application.py
+++ application.py
@@ -23,12 +23,43 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("register.html")
+    return render_template("index.html")
 
+# Registration of users
 @app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
-        if request.form.get("username") and request.form.get("password"):
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # Check whether the user has provided username and password
+        if username and password:
+            usernames = db.execute("SELECT username FROM users")
+
+            # Checking whether username already exists or not
+            for user in usernames:
+                if user.username == username:
+                    return "Username Already Exists, please enter a different username"
+
+            users = db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": username, "password": password})
+            db.commit()
             return "You are registered"
+
         else:
             return render_template("error.html")
+    return render_template("register.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("log_username")
+        password = request.form.get("log_password")
+
+        users = db.execute("SELECT username, password FROM users")
+        for user in users:
+            if user.username == username and user.password == password:
+                return render_template("welcome.html")
+
+        return "username or password in correct"
+
+    return render_template("login.html")
